0efc04ac742cc9de4824aafa4eb4151e619d9778
Harshad347 <harshaddukare347@gmail.com>
2022-03-19 21:39:27 +0530

    Minimum Operations to Halve Array Sum

--- Weekly Contest/Contest -74/Q3_/Q3_minimum_operations_to_halve_array_sum.py
+++ Weekly Contest/Contest -74/Q3_/Q3_minimum_operations_to_halve_array_sum.py
@@ -2,12 +2,12 @@
 
 class Solution:
     def halveArray(self, nums: List[int]) -> int:
-        original_sum = float(sum(nums))
-
-        latest_sum = original_sum
+        sum1 = float(sum(nums))
+        latest_sum = sum1
+        original_sum = sum1/2
         steps = 0
 
-        while latest_sum*2 >= original_sum:
+        while latest_sum >= original_sum:
             nums.sort()
             nums[-1] = float(nums[-1]/2)
             latest_sum = float(sum(nums))
